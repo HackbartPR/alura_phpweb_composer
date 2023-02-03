07cca4999806ddceb84e94c8046c8aee4bbd5da7
HackbartPR <cgharckbart@gmail.com>
2023-02-03 20:09:50 -0300

    $1

--- src/utils/Util.php
+++ src/utils/Util.php
@@ -1,9 +1,11 @@
 <?php
 
-/* class Util
+// phpcs:disable
+class Util
 {
     public static function showMessage()
     {
         echo "Buscando os cursos da URL informada:" . PHP_EOL;
     }
-} */
+}
+// phpcs:enable
